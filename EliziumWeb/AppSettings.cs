4d3575873273eaf263311201b431917b45a68745
Сергей Антипов <Antipych@live.ru>
2019-03-15 21:40:32 +0300

    Add project files.

---
+++ EliziumWeb/AppSettings.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EliziumWeb
+{
+    public class AppSettings
+    {
+        public string StatFolder { get; set; }
+    }
+}
---
+++ EliziumWeb/Controllers/StatController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace EliziumWeb.Controllers
+{
+    public class StatController : Controller
+    {
+        private readonly IOptions<AppSettings> config;
+
+        public StatController(IOptions<AppSettings> config)
+        {
+            this.config = config;
+        }
+
+        [ResponseCache(Duration = 10, Location = ResponseCacheLocation.Any, NoStore = false)]
+        public ActionResult Details(string id, string name)
+        {
+            JObject data = null;
+
+            var f = config.Value.StatFolder;
+
+            var file = new FileInfo($"{f}/{id}.json");
+            if (!file.Exists)
+                return NotFound();
+
+            using (StreamReader jf = file.OpenText())
+            {
+                using (JsonTextReader reader = new JsonTextReader(jf))
+                {
+                    data = (JObject)JToken.ReadFrom(reader);
+                }
+            }
+
+            ViewBag.UserName = name;
+
+            return View(data);
+        }
+    }
+}
